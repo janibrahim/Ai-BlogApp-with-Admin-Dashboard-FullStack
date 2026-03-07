1f51e061fab938d2ba58e428526b902ca5d61be0
janibrahim <janeditz8@gmail.com>
2026-03-08 01:09:31 +0500

    completed Admin Login, Started Admin Dashboard

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className='flex-1 p-4 md:p-10 bg-blue-50/50'>
+        <div className='flex flex-wrap gap-4'>
+            <div>
+              <img src={assets.dashboard_icon_1} alt="" />
+              <div>
+                <p>{}</p>
+              </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
